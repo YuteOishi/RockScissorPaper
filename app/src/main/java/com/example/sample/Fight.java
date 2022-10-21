91aa1447cee5bf0e2c0f0ac3e855ee8e027bd4cf
yu <kch.fefb@gmail.com>
2022-10-21 21:02:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sample/Fight.java
@@ -0,0 +1,131 @@
+package com.example.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class Fight extends AppCompatActivity implements View.OnClickListener {
+    int n;
+    int id;
+    int meId;
+    String me;
+    String enemy;
+    View view;
+    int winNum = 0;
+    int loseNum = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fight);
+
+        findViewById(R.id.rockView).setOnClickListener(this);
+        findViewById(R.id.scissorView).setOnClickListener(this);
+        findViewById(R.id.paperView).setOnClickListener(this);
+
+        findViewById(R.id.enemyRockView).setVisibility(View.INVISIBLE);
+        findViewById(R.id.enemyScissorView).setVisibility(View.INVISIBLE);
+        findViewById(R.id.enemyPaperView).setVisibility(View.INVISIBLE);
+        findViewById(R.id.restart).setVisibility(View.INVISIBLE);
+    }
+
+    public void onClick(View view) {
+        Random random = new Random();
+        hideEnemy();
+        int n = random.nextInt(3);
+        if (n==0) {
+            findViewById(R.id.enemyRockView).setVisibility(View.VISIBLE);
+            enemy = "rock";
+        } else if (n==1) {
+            findViewById(R.id.enemyScissorView).setVisibility(View.VISIBLE);
+            enemy = "scissor";
+        } else {
+            findViewById(R.id.enemyPaperView).setVisibility(View.VISIBLE);
+            enemy = "paper";
+        }
+        if (view.getId() == R.id.rockView) {
+            me = "rock";
+            judge();
+        } else if (view.getId() == R.id.scissorView) {
+            me = "scissor";
+            judge();
+        } else if (view.getId() == R.id.paperView) {
+            me = "paper";
+            judge();
+        }
+    }
+
+    public void onRestart(View view) {
+        hideEnemy();
+        apperMeAll();
+        findViewById(meId).setOnClickListener(this);
+        ((TextView)findViewById(R.id.text1)).setText("  最初はグー！　\n　じゃんけん！  ");
+        findViewById(R.id.restart).setVisibility(View.INVISIBLE);
+    }
+
+    public void onBack(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("WIN", winNum);
+        intent.putExtra("LOSE", loseNum);
+        startActivity(intent);
+    }
+
+    public void hideEnemy(){
+        findViewById(R.id.enemyRockView).setVisibility(View.INVISIBLE);
+        findViewById(R.id.enemyScissorView).setVisibility(View.INVISIBLE);
+        findViewById(R.id.enemyPaperView).setVisibility(View.INVISIBLE);
+    }
+      public void hideMe(){
+        findViewById(R.id.rockView).setVisibility(View.INVISIBLE);
+        findViewById(R.id.scissorView).setVisibility(View.INVISIBLE);
+        findViewById(R.id.paperView).setVisibility(View.INVISIBLE);
+    }
+
+    public void apperMeAll(){
+        findViewById(R.id.rockView).setVisibility(View.VISIBLE);
+        findViewById(R.id.scissorView).setVisibility(View.VISIBLE);
+        findViewById(R.id.paperView).setVisibility(View.VISIBLE);
+    }
+
+    public void apperMe(){
+        meId = getResources().getIdentifier(me + "View", "id", getPackageName());
+        findViewById(meId).setVisibility(View.VISIBLE);
+        findViewById(meId).setOnClickListener(null);
+    }
+
+    public void tie(){
+        ((TextView)findViewById(R.id.text1)).setText("あいこで～");
+    }
+    public void win(){
+        ((TextView)findViewById(R.id.text1)).setText("勝利！！");
+        hideMe();
+        apperMe();
+        findViewById(R.id.restart).setVisibility(View.VISIBLE);
+        winNum++;
+    }
+    public void lose(){
+        ((TextView)findViewById(R.id.text1)).setText("敗北！！");
+        hideMe();
+        apperMe();
+        findViewById(R.id.restart).setVisibility(View.VISIBLE);
+        loseNum++;
+    }
+    public void judge(){
+        if(me==enemy){
+            tie();
+        }
+        else if((me=="rock"&&enemy=="scissor")||(me=="scissor"&&enemy=="paper")||(me=="paper"&&enemy=="rock")){
+            win();
+        }
+        else
+            lose();
+    }
+
+
+}
