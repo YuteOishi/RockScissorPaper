ad92dd3f184012cfbc0c19cfdcc8273eb461f14b
yu <kch.fefb@gmail.com>
2022-10-21 22:06:24 +0900

    bete2 追加：勝ち数、負け数の記録機能 putExtra()、getIntExtra()

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -10,15 +10,26 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int winNum;
+    int loseNum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        winNum = intent.getIntExtra("WIN",0);
+        loseNum = intent.getIntExtra("LOSE",0);
+        ((TextView)findViewById(R.id.winCount)).setText(""+winNum);
+        ((TextView)findViewById(R.id.loseCount)).setText(""+loseNum);
     }
 
     public void onStart(View view) {
         Intent intent = new Intent(this, Fight.class);
+        intent.putExtra("WIN", winNum);
+        intent.putExtra("LOSE", loseNum);
         startActivity(intent);
     }
 
-}
+    }
+
